7a1075299ce108f1072a2ff11a235587c5b3e3fe
mmzare3438-lang <mmzare3438@gmail.com>
2025-10-17 23:49:15 +0330

    Add files via upload

---
+++ T_daneshgah_1/T_daneshgah_1_Temperature/T_daneshgah_1_Temperature/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohammadmahdi_Zare_T1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double C, F;
+            Console.Write("Enter a temp (C): ");
+            C = Double.Parse(Console.ReadLine());
+
+
+            F = C * 1.8 + 32;
+
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
