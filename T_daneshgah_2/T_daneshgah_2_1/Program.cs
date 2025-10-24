586636e3aefef7778493b7591d24292a1d5c1dd1
mmzare3438-lang <mmzare3438@gmail.com>
2025-10-24 17:57:47 +0330

    Add files via upload

---
+++ T_daneshgah_2/T_daneshgah_2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohammadmahdi_Zare_T2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Enter a number: ");
+            a = int.Parse(Console.ReadLine());
+
+
+            if (a % 2 == 0 & a < 10)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
---
+++ T_daneshgah_2/T_daneshgah_2_2/Program.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohammadmahdi_Zare_T2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, a;
+            Console.Write("Enter your desired number: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 7 == 0)
+            {
+                Console.WriteLine("yes!");
+            }
+            else
+            {
+                c = a * 3;
+                Console.WriteLine(c);
+                Console.ReadKey();
+            }
+        }
+    }
+}
