77e589d16eaedf39e263034c9176c473a0f3448a
mmzare3438-lang <mmzare3438@gmail.com>
2025-11-07 19:24:26 +0330

    Add files via upload

---
+++ T_daneshgah_4/T_daneshgah_4_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_daneshgah_4_1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int evenCount = 0;
+            int oddCount = 0;
+
+            Console.WriteLine("Enter 10 numbers:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Enter number " + i + ": ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Results:");
+            Console.WriteLine("Even numbers: " + evenCount);
+            Console.WriteLine("Odd numbers: " + oddCount);
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ T_daneshgah_4/T_daneshgah_4_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_daneshgah_4_2
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<int> positions = new List<int>();
+
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Enter number " + i + ": ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 2)
+                {
+                    positions.Add(i);
+                }
+            }
+
+            if (positions.Count > 0)
+            {
+                Console.Write("Number 2 found at position(s): ");
+                for (int i = 0; i < positions.Count; i++)
+                {
+                    Console.Write(positions[i]);
+                    if (i < positions.Count - 1)
+                        Console.Write(", ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Number 2 not found");
+            }
+        }
+    }
+}
